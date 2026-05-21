b9b700a8c1f989800e10d7b4878cb170fb8e7eef
sushmitatimalsina <sushmitatimalsina602@gmail.com>
2026-05-21 22:54:06 +0545

    feat: add functionality to fetch and display COVID-19 data from API

--- covid_dashboard.py
+++ covid_dashboard.py
@@ -1,3 +1,11 @@
 import requests
 import pandas as pd
 
+url = "https://disease.sh/v3/covid-19/countries"
+response = requests.get(url)
+if response.status_code == 200:
+    data = response.json()
+    df = pd.DataFrame(data)
+    print(df.head())
+else:
+    print(f"Failed to retrieve data. Status code: {response.status_code}")
