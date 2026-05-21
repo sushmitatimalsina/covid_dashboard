66b4cf344df0d681ce154676639110a9197b19c1
sushmitatimalsina <sushmitatimalsina602@gmail.com>
2026-05-21 23:07:08 +0545

    feat: add visualization for top 10 countries by COVID-19 cases

--- covid_dashboard.py
+++ covid_dashboard.py
@@ -1,5 +1,6 @@
 import requests
 import pandas as pd
+import matplotlib.pyplot as plt
 
 url = "https://disease.sh/v3/covid-19/countries"
 response = requests.get(url)
@@ -9,3 +10,31 @@ if response.status_code == 200:
     print(df[['country', 'cases', 'deaths', 'recovered']].head())
 else:
     print(f"Failed to retrieve data. Status code: {response.status_code}")
+
+
+clean_df = df[['country', 'cases', 'deaths', 'recovered']]
+clean_df = clean_df.rename(columns={
+    'country': 'Country',
+    'cases': 'Total Cases',
+    'deaths': 'Total Deaths',
+    'recovered': 'Total Recovered'
+})
+
+clean_df= clean_df.sort_values(by='Total Cases', ascending=False).reset_index(drop=True)
+print(clean_df.head(1000))
+
+top_10 = clean_df.head(10)
+
+plt.figure(figsize=(10,5))
+
+plt.bar(top_10['Country'], top_10['Total Cases'])
+
+plt.title("Top 10 Countries by COVID Cases")
+
+plt.xlabel("Country")
+
+plt.ylabel("Total Cases")
+
+plt.xticks(rotation=45)
+
+plt.show()
